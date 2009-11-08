c4e845b6334382af950ca26f1b0de580d281c74b
Asa Hopkins <asa@MBP13.local>
2009-11-08 10:58:09 -0800

    production domain_name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,4 +17,4 @@ config.action_controller.perform_caching             = true
 # Disable delivery errors, bad email addresses will be ignored
 # config.action_mailer.raise_delivery_errors = false
 
-DOMAIN_NAME = 'http://localhost:3000/'
+DOMAIN_NAME = 'http://tinythom.as'
